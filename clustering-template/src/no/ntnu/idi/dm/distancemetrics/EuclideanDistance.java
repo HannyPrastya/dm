562725438a9625a248d9dbc1686c1f8c9a3405cc
Ole Rønning <ole.ronning@myessential.net>
2013-04-15 11:19:50 +0200

    Implements EuclideanDistance

--- clustering-template/src/no/ntnu/idi/dm/distancemetrics/EuclideanDistance.java
+++ clustering-template/src/no/ntnu/idi/dm/distancemetrics/EuclideanDistance.java
@@ -7,8 +7,11 @@ package no.ntnu.idi.dm.distancemetrics;
 public class EuclideanDistance {
 
 	public double distance(double[] vector1, double[] vector2) {
-		double dist = 0;
-		//TODO
+		double dist = 0d;
+
+        for (int i=0; i < vector1.length; i++) {
+            dist = dist + Math.pow((vector1[i] - vector2[i]), 2);
+        }
 		return Math.sqrt(dist);
 	}
 
